7b3f43e5f5be81a36e9e8f34124e5e8a8677dbf9
dennisba-ibinex <dennis.ba@ibinex.tech>
2018-05-23 18:47:33 +0800

    carousel h5 minor test

--- resources/views/partials/home-carousel.blade.php
+++ resources/views/partials/home-carousel.blade.php
@@ -4,7 +4,7 @@
     <div class="carousel-item active">
       <img class="d-block w-100" src="http://cdn.home-designing.com/wp-content/uploads/2015/11/textured-sofa.jpg" alt="First slide">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Hello, this is a test header test</h5>
+        <h5>Hello, it is a great day today</h5>
         <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
         <div class="carousel-border-triangle">
           <span></span>
@@ -15,7 +15,7 @@
     <div class="carousel-item">
       <img class="d-block w-100" src="http://www.yustusa.com/9/2015/08/natural-interior-design-of-the-modern-custom-log-homes-that-has-wooden-floor-can-be-decor-with-warm-lighting-can-add-the-beauty-inside-with-wooden-floor-inside.jpg" alt="Second slide">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Hello, this is a test header test</h5>
+        <h5>Hi, I hope you are doing fine</h5>
         <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
         <div class="carousel-border-triangle">
           <span></span>
@@ -26,7 +26,7 @@
     <div class="carousel-item">
       <img class="d-block w-100" src="http://hawaiiopenbudget.com/img/2018/05/cabinets-remodel-decorating-kitchens-ideas-outdoor-dining-photos-kitchen-with-apartments-modern-decorate-granite-new-budget-countertop-floor-homeinteriors-organization-liances-desi.jpg" alt="Third slide">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Hello, this is a test header test</h5>
+        <h5>Let's build great stuff together!</h5>
         <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
         <div class="carousel-border-triangle">
           <span></span>
