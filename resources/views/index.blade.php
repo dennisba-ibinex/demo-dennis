094f22e11c9c8c646f22d79f5197d112ecfe9ad7
dennisba-ibinex <dennis.ba@ibinex.tech>
2018-05-23 18:30:16 +0800

    homepage-carousel and welcome text initial commit and minor header adjustments

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
+  @include('partials.home-carousel')
+  <div class="welcome-text">
+    <p>It is with great pleasure that {{ get_bloginfo('name') }} welcomes you to El Segundo, and to her website. With years of experience in the market, she knows how crucial it is for you to find relevant, up-to-date information. The search is over. Her website is designed to be your one-stop shop for real estate in El Segundo.This is the moment that you should enjoy the most; looking at the available properties in El Segundo; imagining yourself living in the home that you have always dreamed about. You don’t want just another database that gives you rehashed property descriptions. You want to walk around the neighborhood from the comfort of your own home. You want to get a clear picture about life in El Segundo.</p>
+  </div>
 @endsection
---
+++ resources/views/partials/home-carousel.blade.php
@@ -0,0 +1,46 @@
+
+<div id="homepage-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="http://cdn.home-designing.com/wp-content/uploads/2015/11/textured-sofa.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Hello, this is a test header test</h5>
+        <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
+        <div class="carousel-border-triangle">
+          <span></span>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://www.yustusa.com/9/2015/08/natural-interior-design-of-the-modern-custom-log-homes-that-has-wooden-floor-can-be-decor-with-warm-lighting-can-add-the-beauty-inside-with-wooden-floor-inside.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Hello, this is a test header test</h5>
+        <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
+        <div class="carousel-border-triangle">
+          <span></span>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://hawaiiopenbudget.com/img/2018/05/cabinets-remodel-decorating-kitchens-ideas-outdoor-dining-photos-kitchen-with-apartments-modern-decorate-granite-new-budget-countertop-floor-homeinteriors-organization-liances-desi.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Hello, this is a test header test</h5>
+        <h1 class="carousel-caption-2">{{ get_bloginfo('name') }} REAL ESTATE</h1>
+        <div class="carousel-border-triangle">
+          <span></span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <a class="carousel-control-prev" href="#homepage-carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#homepage-carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
