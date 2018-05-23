e3b627a32a4e693efbaaadff108b0b8e5cb7b414
dennisba-ibinex <dennis.ba@ibinex.tech>
2018-05-23 14:57:39 +0800

    initial header/navbar design

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,31 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
-</header>
+<div class="header-bg">
+	<div class="bg-left"></div>
+	<div class="bg-right"></div>
+	<header class="header-wrap banner">
+		<div class="dd-header-inner">
+			<div class="dd-site-name dd-header-block">
+				<a href="{{ get_site_url() }}">
+					<span>
+						{{ get_bloginfo('name') }}
+					</span>
+				</a>
+			</div>
+
+			<div class="dd-site-tagline dd-header-block">
+				<span>
+					{{ get_bloginfo('description') }}
+				</span>
+			</div>
+
+			<div class="dd-site-logo dd-header-block">
+				<a href="{{ get_site_url() }}">
+					<img src="https://i.imgur.com/gV9TrRM.png" alt="Company Logo">
+				</a>
+			</div>
+		</div>
+
+		<nav class="nav-wrap">
+			{{ wp_nav_menu('primary_navigation') }}
+		</nav>
+	</header>
+</div>
